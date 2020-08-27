d685e2a61837e1b48cd71c25ee1090080f4ae29c
jpsacheti <jpsacheti@gmail.com>
2020-08-26 21:41:39 -0300

    Fix folders issues and add index page

--- pet-clinic-data/src/main/java/guru/springframework/spgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/spgpetclinic/services/OwnerService.java
@@ -3,5 +3,5 @@ package guru.springframework.spgpetclinic.services;
 import guru.springframework.spgpetclinic.model.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long> {
-	Owner findByLastName(String lastName);
+
 }
